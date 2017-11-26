f8c7c23319cb5571a9a6202603fdebdae7865fd0
owenwilson <owenwilson76467963@gmail.com>
2017-11-26 01:42:59 -0400

    boostrap integred

---
+++ index.php
@@ -0,0 +1,50 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <!--<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <title>boostrap</title>
+    </head>
+    <body>
+
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <ul class="nav navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link on the Right</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 well">
+                <h1>Si vemos esto todo salido correctamente</h1>
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery (necesario para los complementos de JavaScript de Bootstrap) -->
+    <script src="jquery-3.2.1.min.js"></script>
+    <!-- Incluye todos los complementos compilados (a continuación) o incluye archivos individuales según sea necesario -->
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
