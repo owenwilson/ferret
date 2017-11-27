41c0fde3e02a6cb7e18b02cab9cb221b466c00b4
owenwilson <owenwilson76467963@gmail.com>
2017-11-27 01:29:59 -0400

    favicon upload

---
+++ js/Login.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#Iniciar").click(function(){
+        var Usuario = $('#usuario').val();
+        var Contrasena = $('#contrasena').val();
+        console.log(Usuario,Contrasena);
+    });
+});
