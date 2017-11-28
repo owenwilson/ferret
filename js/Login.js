1a6e1614ba0c86e441f2430e2db33bee57661dfe
owenwilson <owenwilson76467963@gmail.com>
2017-11-28 01:00:31 -0400

    create archive login.js

--- js/Login.js
+++ js/Login.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
     $("#Iniciar").click(function(){
         var Usuario = $('#usuario').val();
         var Contrasena = $('#contrasena').val();
-        console.log(Usuario,Contrasena);
+        //console.log(Usuario,Contrasena);
+        $ajax({
+            type:"POST",
+            datatype:'json',
+            url:'php/LoginAjax.php',
+            data:{Usuario:Usuario,Contrasena:Contrasena},
+            success:function(response){
+            if(response.)    
+            }
+        });
     });
 });
