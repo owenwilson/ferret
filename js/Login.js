c24a277ad38f0c905fe4f60438e685c48d8905b9
owenwilson <owenwilson76467963@gmail.com>
2017-12-01 11:42:34 -0400

    first page integrated

--- js/Login.js
+++ js/Login.js
@@ -9,8 +9,15 @@ $(document).ready(function(){
             url:'php/LoginAjax.php',
             data:{Usuario:Usuario,Contrasena:Contrasena},
             success:function(response){
-            if(response.)    
+            if(response.respuesta==true){
+                $("#mensaje").html(response.mensaje);
+                window.location='principal.php';
+            }else{
+                $("#mensaje").html(response.mensaje);
             }
+        },error:function(){
+            alert('Error General en el sistema');
+        }
         });
     });
 });
---
+++ principal.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>bienvenido</title>
+    </head>
+    <body>
+        <h1>Bienbenidos</h1>
+    </body>
+</html>
